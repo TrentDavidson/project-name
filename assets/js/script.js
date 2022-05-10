7b615a96b7314448523621c95a0d759df8da801c
Andrew Faugno <andrewfaugno825@gmail.com>
2022-05-09 20:30:23 -0400

    added hourly option and geocode api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
-// var apiUrlGeo = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York"
-var apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York"
+var apiUrlHourly = "https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode,windspeed_10m,winddirection_10m&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York"
+var cityName = "greenwich"
+var apiGeo = "https://geocoding-api.open-meteo.com/v1/search?name=" + cityName + "&count=1"
 
+var cityGeoLocation = function () {
+    fetch(apiGeo)
+        .then(response => response.json())
+        .then(data => {
+            var location = {
+                longitude: data.results[0].longitude,
+                latitude: data.results[0].latitude,
+                city: data.results[0].name
+            };
+            
+            weatherDataDaily(location)
+        })
+};
 
-var weatherData = function() {
+var weatherDataDaily = function(location) {
+    console.log(location)
+    var longitude = location.longitude
+    var latitude = location.latitude
+    var apiUrlDaily = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York"
+
     // retrieves data from apiUrl
-    fetch(apiUrl)
+    fetch(apiUrlDaily)
         .then(response => response.json())
         .then(data => {
             // logs available data
@@ -35,16 +54,26 @@ var weatherData = function() {
 
                 // date , average temp, wind speed , sunrise to sunset 
                 console.log(date + " ", averageTemp + ", " + windSpeed + ", ", sunrise + " -> " + sunset);
-
             }
-
         }) 
-        
-        
-
 }
 
-// calls weatherData function (geocode will call later and enter the long and lat)
-weatherData();
+var weatherDataHourly = function() {
+    fetch(apiUrlHourly)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            for (var i = 0; i < 6; i++) {
+                var unix = data.hourly.time[i]
+                var time = moment(unix);
+                console.log(time)
+                console.log(unix)
+            }
+        })
+}
+
+// calls daily weatherData function (geocode will call later and enter the long and lat)
+// weatherDataHourly();
+cityGeoLocation();
 
 
